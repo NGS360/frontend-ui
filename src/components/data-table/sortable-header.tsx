8c26d6a73309a39e7e71d89633932c891315be41
Eric Davis <31807001+EricSDavis@users.noreply.github.com>
2025-09-08 11:14:55 -0400

    Make sortable header show sort controls on hover (if they are not already applied).

--- src/components/data-table/sortable-header.tsx
+++ src/components/data-table/sortable-header.tsx
@@ -10,13 +10,13 @@ interface SortableHeaderProps<TData, TValue> {
 export const SortableHeader = <TData, TValue>({ column, name }: SortableHeaderProps<TData, TValue>) => {
   return (
     <div
-      className='flex items-center cursor-pointer w-full h-full'
+      className='group flex items-center cursor-pointer w-full h-full'
       onClick={() => column.toggleSorting()}
     >
       {name}
-      {!column.getIsSorted() && (<ArrowUpDown className='ml-2 h-4 w-4' />)}
+      {!column.getIsSorted() && (<ArrowUpDown className='ml-2 h-4 w-4 opacity-0 group-hover:opacity-100 transition-opacity' />)}
       {column.getIsSorted() === 'asc' ? (<ArrowDown className='ml-2 h-4 w-4' />) : (null)}
-      {column.getIsSorted() === 'desc' ? (<ArrowUp className='ml-2 h-4 w-4' />) : (null)}
+      {column.getIsSorted() === 'desc' ? (<ArrowUp className='ml-2 h-4 w-4 ' />) : (null)}
     </div>
   )
 }
